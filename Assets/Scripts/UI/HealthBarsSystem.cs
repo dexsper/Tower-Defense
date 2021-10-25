4800c123af47fcdbad6405ab6f7a71cf718651d7
anonymuspro <anonymuspro@bk.ru>
2021-10-25 22:57:42 +0600

    Swordman update
    New Swordsman Unit
    Edit Goblin characteristics

---
+++ Assets/Scripts/UI/HealthBarsSystem.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarsSystem : MonoBehaviour
+{
+    private Dictionary<Entity, HealthBar> healthBars = new Dictionary<Entity, HealthBar>();
+
+    [SerializeField]
+    private HealthBar healthBarPrefab;
+
+    [SerializeField]
+    private Transform healthBarsParent;
+
+    private void Awake()
+    {
+        Entity.OnEntityDestroy += RemoveHealthBar;
+        Entity.OnEntityCreate += CreateHealthBar;
+    }
+
+    private void RemoveHealthBar(Entity entity)
+    {
+        if (healthBars.ContainsKey(entity))
+        {
+            var healthBar = healthBars[entity];
+
+            if (healthBar)
+                Destroy(healthBar.gameObject);
+
+            healthBars.Remove(entity);
+        }
+    }
+
+    private void CreateHealthBar(Entity entity)
+    {
+        if(!healthBars.ContainsKey(entity))
+        {
+            var healthBar = Instantiate(healthBarPrefab, healthBarsParent);
+            healthBars.Add(entity, healthBar);
+            healthBar.Init(entity);
+        }
+    }
+}
